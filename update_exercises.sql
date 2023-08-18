27e2765cda5615b027df380119bb180649f61a06
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-18 11:54:29 -0500

    finished problem 3 of exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,3 +1,15 @@
 SELECT codeup_test_db.albums.name FROM codeup_test_db.albums;
+UPDATE codeup_test_db.albums
+SET codeup_test_db.albums.sales = sales * 10;
+SELECT codeup_test_db.albums.* FROM albums;
 
-SELECT codeup_test_db.albums.name < 1980 FROM codeup_test_db.albums;
+SELECT codeup_test_db.albums.name FROM albums WHERE release_date < 1980;
+UPDATE codeup_test_db.albums
+SET codeup_test_db.albums.release_date = release_date - 100 WHERE release_date < 1980;
+SELECT codeup_test_db.albums.* FROM albums;
+
+SELECT codeup_test_db.albums.name FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET codeup_test_db.albums.artist = 'Samuel Jackson' WHERE artist = 'Michael Jackson';
+SELECT codeup_test_db.albums.* FROM albums;
+
