8de3bf70526d3bcdf97ae281259ea3940516d6e2
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-23 10:53:27 -0500

    started quiz application practice

--- join_exercises.sql
+++ join_exercises.sql
@@ -11,18 +11,20 @@ ON employees.emp_no = dept_manager.emp_no
 JOIN departments
 ON dept_manager.dept_no = departments.dept_no
 WHERE to_date = '9999-01-01'
-ORDER BY dept_name ASC;
+ORDER BY dept_name
+;
 
-DESCRIBE employees
+DESCRIBE employees;
 
-SELECT departments.dept_name AS "Department Name", CONCAT(employees.first_name, ' ', employees.last_name) AS "Department Manager"
-FROM employees
-JOIN dept_manager
-ON employees.emp_no = dept_manager.emp_no
-JOIN departments
-ON dept_manager.dept_no = departments.dept_no
-WHERE employees.employees.gender = 'F' AND to_date = '9999-01-01'
-ORDER BY dept_name ASC;
+SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager"
+FROM departments d
+JOIN dept_manager dm
+ON d.dept_no = dm.dept_no
+JOIN employees e
+ON dm.emp_no = e.emp_no
+WHERE e.gender = 'F' AND to_date = '9999-01-01'
+ORDER BY dept_name
+;
 
 DESCRIBE titles;
 
@@ -30,27 +32,29 @@ SHOW TABLES;
 
 DESCRIBE current_dept_emp;
 
-SELECT titles.title AS "title", COUNT(employees.emp_no) AS "Total"
-FROM titles
-LEFT JOIN dept_emp ON titles.emp_no = dept_emp.emp_no
-JOIN employees ON dept_emp.emp_no = employees.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE employees.departments.dept_name = 'Customer Service' AND titles.to_date = '9999-01-01' AND dept_emp.to_date = '9999-01-01'
-GROUP BY titles.title
-ORDER BY COUNT(employees.employees.emp_no) DESC;
+SELECT t.title AS "title", COUNT(t.emp_no) AS "Total"
+FROM titles t
+        JOIN dept_emp de
+            ON t.emp_no = de.emp_no
+        JOIN departments d
+            ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service' AND t.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
+GROUP BY t.title
+ORDER BY COUNT(t.emp_no) DESC;
 
 DESCRIBE salaries;
 
-SELECT departments.dept_name AS "Department Name", CONCAT(employees.first_name, ' ', employees.last_name) AS "Department Manager", salaries.salary AS "Salary"
-FROM employees
-         JOIN dept_manager
-              ON employees.emp_no = dept_manager.emp_no
-         JOIN departments
-              ON dept_manager.dept_no = departments.dept_no
-        JOIN salaries
-            ON salaries.emp_no = dept_manager.emp_no
-WHERE dept_manager.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01'
-ORDER BY dept_name ASC;
+SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager", s.salary AS "Salary"
+FROM employees e
+         JOIN dept_manager dm
+              ON e.emp_no = dm.emp_no
+         JOIN departments d
+              ON dm.dept_no = d.dept_no
+        JOIN salaries s
+            ON dm.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY dept_name
+;
 
 DESCRIBE dept_manager;
 
@@ -60,13 +64,19 @@ DESCRIBE TITLES;
 
 DESCRIBE current_dept_emp;
 
-SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS Employee, departments.dept_name as "Department Name", CONCAT(Managers.first_name, ' ', Managers.last_name) AS Manager
-FROM employees
-        JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-        JOIN departments ON dept_emp.dept_no = departments.dept_no
-        JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
-        JOIN employees AS Managers ON dept_manager.emp_no = Managers.emp_no
-WHERE dept_emp.to_date = '9999-01-01' AND dept_manager.to_date = '9999-01-01';
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee, d.dept_name as "Department Name", CONCAT(Managers.first_name, ' ', Managers.last_name) AS Manager
+FROM employees e
+        JOIN dept_emp de
+            ON e.emp_no = de.emp_no
+        JOIN departments d
+            ON de.dept_no = d.dept_no
+        JOIN dept_manager dm
+            ON d.dept_no = dm.dept_no
+        JOIN employees AS Managers
+            ON dm.emp_no = Managers.emp_no
+WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+ORDER BY d.dept_name, e.emp_no
+;
 
 
 
---
+++ quiz-database-practice.sql
@@ -0,0 +1,6 @@
+# First we create the database:
+CREATE DATABASE quiz_db;
+
+# Then we enter the database to build tables:
+USE quiz_db;
+
